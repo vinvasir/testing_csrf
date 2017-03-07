e9c4b14483cb1beb2dbcd48a0ea4f8fd2a077cb0
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-03-07 16:30:21 -0500

    add express-session, switch the view engine to ejs, and use the csrf protection middleware. one form does not contain the token and doesn't work. the other does contain the token, and does work

--- app.js
+++ app.js
@@ -2,19 +2,59 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const csurf = require('csurf');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const csrfProtection = csurf({cookie: false});
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// Express session
+app.use(session({
+  secret: 'fjei;awhg;hewro',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+const parseForm = bodyParser.urlencoded({extended: false});
+app.use(parseForm);
+app.use(logger('dev'));
+
+
+// once express session and body parser are used,
+// you can use the csrfProtection module
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+	if (req.body) {
+		console.log(req.body);
+	}
+	next();
+});
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html');
+	res.render('index', {csrfToken: req.csrfToken()});
 });
+
+app.post('/', (req, res) => {
+	res.status(200).json({
+		status: "Success!",
+		message: "Good CSRF token!"
+	});
+});
+
+// error handler
+// app.use(function (err, req, res, next) {
+//   if (err.code !== 'EBADCSRFTOKEN') return next(err)
+
+//   // handle CSRF token errors here
+//   res.status(403)
+//   res.send('bad csrf')
+// });
 
 app.listen(port, () => {
 	console.log('Server started on port ' + port);
